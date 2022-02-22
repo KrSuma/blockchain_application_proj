a61786baec9ecfd66cc86a8cbdb57b695c2e7f86
jaesu <jaesun90@gmail.com>
2022-02-22 12:19:33 +0900

    update

--- backend/tests/blockchain/test_block.py
+++ backend/tests/blockchain/test_block.py
@@ -1,4 +1,4 @@
-from backend.blockchain.block import Block
+from backend.blockchain.block import Block, GENESIS_DATA
 
 
 def test_mine_block():
@@ -8,9 +8,20 @@ def test_mine_block():
 
     assert isinstance(block, Block)
     assert block.data == data
+    assert block.last_hash == last_block.hash
+    assert block.hash[0:block.difficulty] == '0' * block.difficulty
 
 
 def test_genesis():
     genesis = Block.genesis()
 
     assert isinstance(genesis, Block)
+    for key, value in GENESIS_DATA.items():
+        getattr(genesis, key) == value
+
+
+def test_quick_mined_block():
+    last_block = Block.mine_block(Block.genesis(), 'foo')
+    mined_block = Block.mine_block(last_block, 'bar')
+
+    assert mined_block.difficulty == last_block.difficulty + 1
